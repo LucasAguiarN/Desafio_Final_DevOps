0349cf5cdd9c2510e0541f2698d8ac79bab7445e
Lucas-Aguiar-Nunes <lucas_aguiar_nunes@outlook.com>
2025-11-21 23:05:30 -0300

    add novos testes

--- test_app.py
+++ test_app.py
@@ -23,6 +23,29 @@ class APITestCase(unittest.TestCase):
     def test_protected_no_token(self):
         response = self.client.post('/protected')
         self.assertEqual(response.status_code, 401)
+    
+    def test_protected_with_token(self):
+        login_response = self.client.get('/login')
+        token = login_response.json['access_token']
+        response = self.client.post(
+            '/protected',
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"message": "Protected route"})
+
+    def test_get_items(self):
+        response = self.client.get('/items')
+        self.assertEqual(response.status_code, 200)
+        data = response.json
+        self.assertIn("items", data)
+        self.assertIsInstance(data["items"], list)
+
+    def test_get_items_content(self):
+        response = self.client.get('/items')
+        self.assertEqual(response.status_code, 200)
+        data = response.json
+        self.assertListEqual(data["items"], ["item1", "item2", "item3"])
 
 if __name__ == '__main__':
     unittest.main()
